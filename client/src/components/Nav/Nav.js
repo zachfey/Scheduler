231c5e9f0abb49cc2ce5d786c837bd3ceece988e
zachfey <zachary.fey@gmail.com>
2019-07-15 20:03:12 -0400

    Revert "Added OAuth user authentication"
    This reverts commit 1f8854baf493e8d5331bda3014346a55d8a8a882.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,27 +1,11 @@
 import React from "react";
-import { useAuth0 } from "../../react-auth0-wrapper";
 
 function Nav() {
-  const {isAuthenticated, loginWithRedirect, logout } = useAuth0();
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
         React Reading List
       </a>
-      <div>
-      {!isAuthenticated && (
-        <button
-          onClick={() =>
-            loginWithRedirect({})
-          }
-        >
-          Log in
-        </button>
-      )}
-
-      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
-    </div>
-  );
     </nav>
   );
 }
