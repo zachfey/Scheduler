0257cde76b0148de6bbe1f9d44544b7e091ff49e
zachfey <zachary.fey@gmail.com>
2019-07-20 01:33:06 -0400

    npm run build

--- react-ui/server.js
+++ react-ui/server.js
@@ -24,7 +24,7 @@ app.use(
 )
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 
 // Initialize passport session
