8af2663a03bfe416e8cbf2ee1fb428874dd3ab7e
zachfey <zachary.fey@gmail.com>
2019-07-20 01:23:13 -0400

    deploying

---
+++ react-ui/client/public/precache-manifest.a4d14d2d59dccfbcc0c6c7718bd7156a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "873b7253f96070736a8d82de7c6eed37",
+    "url": "/static/media/Raft1Logo.873b7253.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "97fe70f5f3bc00f095eb",
+    "url": "/static/js/main.e5a05533.chunk.js"
+  },
+  {
+    "revision": "eb83fe097dc97135aab9",
+    "url": "/static/js/2.8d276994.chunk.js"
+  },
+  {
+    "revision": "97fe70f5f3bc00f095eb",
+    "url": "/static/css/main.5ed96f7f.chunk.css"
+  },
+  {
+    "revision": "eb83fe097dc97135aab9",
+    "url": "/static/css/2.ab8d3d34.chunk.css"
+  },
+  {
+    "revision": "7544a8e9f20b1df928528749675e2528",
+    "url": "/index.html"
+  }
+];
