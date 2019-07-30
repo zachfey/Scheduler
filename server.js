dffea0cc7f8d7701d470d4c8009fc19072a93223
zachfey <zachary.fey@gmail.com>
2019-07-29 23:07:54 -0400

    changed dirname to client/build from /build

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get('/*', function (req, res) {
     console.log('serving up index.html')
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
   })
 }
 
