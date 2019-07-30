d97853d28e45d54c5a4f981663795c102e5d8f43
zachfey <zachary.fey@gmail.com>
2019-07-29 22:37:30 -0400

    added app.get /* route to try to fix deployment

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ app.use(
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  })
 }
 
 // Initialize passport session
